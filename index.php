c15707934941f84864cf99835e6dd2892c4a2afd
shariqmanzur <shariqmanzur@gmail.com>
2022-09-16 11:20:02 +0500

    Setting up WebSocket Server

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
   echo "Live Video Chat Application";
 ?>
+<script>
+  var conn = new WebSocket('ws://localhost:8080');
+  conn.onopen = function(e) {
+      console.log("Connection established!");
+  };
+
+  conn.onmessage = function(e) {
+      console.log(e.data);
+  };
+</script>
